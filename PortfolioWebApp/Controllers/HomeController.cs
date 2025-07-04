a4aa19e79bd4a9490ff3f0b12f05db40a649b06e
kerem aydın <krmydn7@gmail.com>
2025-07-04 16:46:55 +0300

    Veritabanına kayıt işlemi eklendi

--- PortfolioWebApp/Controllers/HomeController.cs
+++ PortfolioWebApp/Controllers/HomeController.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using PortfolioWebApp.Models;
+using PortfolioWebApp.Data;
 
 namespace PortfolioWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Contact contact)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                _context.Contacts.Add(contact);
+                _context.SaveChanges();
+                ViewBag.Message = "Mesajınız başarıyla gönderildi!";
+            }
 
             return View();
         }
---
+++ PortfolioWebApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using PortfolioWebApp.Models;
+
+namespace PortfolioWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
